a200b9c8bb630c1c95eb2f807a51dfaaf9cd9dd2
Vinod Kulkarni <2379080+codevin@users.noreply.github.com>
2020-12-26 18:11:02 +0530

    Generic compilation ...

--- go-server/build.sh
+++ go-server/build.sh
@@ -2,13 +2,13 @@
 
 # Compile for x86, normal compilation.
 echo "Building cmdline"
-env GOOS=linux go build -ldflags="-extldflags=-static -X server.minversion=`date -u +.%Y%m%d.%H%M%S`" -o cmdline cmdline.go 
+env go build -ldflags="-extldflags=-static -X server.minversion=`date -u +.%Y%m%d.%H%M%S`" -o cmdline cmdline.go 
 
 echo "Linking update to cmdline. It is same app, but used for sending update rather than new widget request."
 ln -sf cmdline update
 
 echo "Building script-manager"
-env GOOS=linux go build -ldflags="-extldflags=-static -X server.minversion=`date -u +.%Y%m%d.%H%M%S`" -o script-manager main.go
+env go build -ldflags="-extldflags=-static -X server.minversion=`date -u +.%Y%m%d.%H%M%S`" -o script-manager main.go
 
 if [ x$1 == x"run" ]
 then
